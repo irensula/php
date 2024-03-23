6986a21e7dd1e433d166756596df6df86ebb2185
irensula <irinawebdev@gmail.com>
2024-03-23 23:19:53 +0200

    Add files via upload

--- phpharjoitukset7/index.php
+++ phpharjoitukset7/index.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
-    <!-- css -->
-    <link rel="stylesheet" href="styles/reset.css">
-    <link rel="stylesheet" href="styles/styles.css">
-    
-    <title>Document</title>
-</head>
-<body>
+<?php include "header.php"; ?>
+
     <div class="container">
         <h1 class="title">Tiedoston kirjoittaminen</h1>
         <form action="index.php" method="get">
