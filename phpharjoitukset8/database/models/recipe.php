246c29193b9cd0a6bf28bff450d7819bdb930719
irensula <irinawebdev@gmail.com>
2024-04-12 21:47:49 +0300

    Add files via upload

---
+++ phpharjoitukset8/database/models/recipe.php
@@ -0,0 +1,51 @@
+<?php
+require_once "../database/connection.php";
+
+function getAllRecipes(){
+    $pdo =connectDB();
+    $sql = "SELECT * FROM recipes";
+    $stm = $pdo->query($sql);
+    $all = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $all;
+}
+
+function addRecipe($name, $additionDate, $category, $ingredients, $preparation, $userID){
+    $pdo =connectDB();
+    $data = [$name, $additionDate, $category, $ingredients, $preparation, $userID];
+    $sql = "INSERT INTO recipes (name, additionDate, category, ingredients, preparation, userID) VALUES(?,?,?,?,?,?)";
+    $stm=$pdo->prepare($sql);
+    return $stm->execute($data);
+}
+
+function getRecipeById($id){
+    $pdo = connectDB();
+    $sql = "SELECT * FROM recipes WHERE recipeID=?";
+    $stm= $pdo->prepare($sql);
+    $stm->execute([$id]);
+    $all = $stm->fetch(PDO::FETCH_ASSOC);
+    return $all;
+}
+
+function deleteRecipe($id){
+    $pdo = connectDB();
+    $sql = "DELETE FROM recipes WHERE recipeID=?";
+    $stm=$pdo->prepare($sql);
+    return $stm->execute([$id]);
+}
+
+function updateRecipe($name, $additionDate, $category, $ingredients, $preparation, $recipeID){
+    $pdo = connectDB();
+    $data = [$name, $additionDate, $category, $ingredients, $preparation, $recipeID];
+    $sql = "UPDATE recipes SET name = ?, additionDate = ?, category = ?, ingredients = ?, preparation = ? WHERE recipeID = ?";
+    $stm = $pdo->prepare($sql);
+    return $stm->execute($data);
+}
+
+function getCategoryRecipes($category){
+    $pdo = connectDB();
+    $sql = "SELECT * FROM recipes WHERE category = ?";
+    $stm=$pdo->prepare($sql);
+    $stm->execute(array($category));
+    $all = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $all;
+} 
