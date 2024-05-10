afe23137f01b5ad8d98424f78d761029e8b478cb
irensula <irinawebdev@gmail.com>
2024-05-10 13:45:59 +0300

    Delete views directory

--- views/contacts.view.php
+++
@@ -1,8 +0,0 @@
-<?php require "partials/header_.php"; ?>
-    <h2>Yhteystiedot</h2>
-    <div class="contacts">
-        <p><span class="bold">Osoite: </span>Makukatu, 12, Tampere, 33300</p>
-        <p><span class="bold">Email: </span>reseptit@gmail.com</p>
-        <p><span class="bold">Puhelinnumero: </span>050-123-45-67</p>
-    </div>
-<?php require "partials/footer.php"; ?>
