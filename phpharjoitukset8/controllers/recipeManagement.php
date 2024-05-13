134e5c426f3726fdf304c287118ec75ce04b37ed
irensula <irinawebdev@gmail.com>
2024-05-13 14:33:19 +0300

    Add files via upload

--- phpharjoitukset8/controllers/recipeManagement.php
+++ phpharjoitukset8/controllers/recipeManagement.php
@@ -137,71 +137,71 @@ function deleteRecipeController(){
 
 
 //  UPLOADING IMAGES
-if(isset($_FILES["fileToUpload"])) {
-    $target_dir = "images/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// if(isset($_FILES["fileToUpload"])) {
+//     $target_dir = "images/";
+//     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+//     $uploadOk = 1;
+//     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-      if($check !== false) {
-        echo "<div class='img-message'>";
-        echo "File is an image - " . $check["mime"] . ".";
-        echo "</div>";
-        $uploadOk = 1;
-      } else {
-        echo "<div class='img-message'>";
-        echo "File is not an image.";
-        echo "</div>";
-        $uploadOk = 0;
-      }
-    }
+//     // Check if image file is a actual image or fake image
+//     if(isset($_POST["submit"])) {
+//       $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+//       if($check !== false) {
+//         echo "<div class='img-message'>";
+//         echo "File is an image - " . $check["mime"] . ".";
+//         echo "</div>";
+//         $uploadOk = 1;
+//       } else {
+//         echo "<div class='img-message'>";
+//         echo "File is not an image.";
+//         echo "</div>";
+//         $uploadOk = 0;
+//       }
+//     }
     
-    // Check if file already exists
-    if (file_exists($target_file)) {
-      echo "<div class='img-message'>";
-      echo "Sorry, file already exists.";
-      echo "</div>";
-      $uploadOk = 0;
-    }
+//     // Check if file already exists
+//     if (file_exists($target_file)) {
+//       echo "<div class='img-message'>";
+//       echo "Sorry, file already exists.";
+//       echo "</div>";
+//       $uploadOk = 0;
+//     }
     
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 2000000) {
-      echo "<div class='img-message'>";
-      echo "Sorry, your file is too large.";
-      echo "</div>";
-      $uploadOk = 0;
-    }
+//     // Check file size
+//     if ($_FILES["fileToUpload"]["size"] > 2000000) {
+//       echo "<div class='img-message'>";
+//       echo "Sorry, your file is too large.";
+//       echo "</div>";
+//       $uploadOk = 0;
+//     }
     
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" && $imageFileType != "avif" ) {
-      echo "<div class='img-message'>";
-      echo "Sorry, only JPG, JPEG, PNG, AVIF & GIF files are allowed.";
-      echo "</div>";
-      $uploadOk = 0;
-    }
+//     // Allow certain file formats
+//     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+//     && $imageFileType != "gif" && $imageFileType != "avif" ) {
+//       echo "<div class='img-message'>";
+//       echo "Sorry, only JPG, JPEG, PNG, AVIF & GIF files are allowed.";
+//       echo "</div>";
+//       $uploadOk = 0;
+//     }
     
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-      echo "<div class='img-message'>";
-      echo "Sorry, your file was not uploaded.";
-      echo "</div>";
-    // if everything is ok, try to upload file
-    } else {
-      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "<div class='img-message'>";
-        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-        echo "</div>";
-      } else {
-        echo "<div class='img-message'>";
-        echo "Sorry, there was an error uploading your file.";
-        echo "</div>";
-      }
-    }
-      }
+//     // Check if $uploadOk is set to 0 by an error
+//     if ($uploadOk == 0) {
+//       echo "<div class='img-message'>";
+//       echo "Sorry, your file was not uploaded.";
+//       echo "</div>";
+//     // if everything is ok, try to upload file
+//     } else {
+//       if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+//         echo "<div class='img-message'>";
+//         echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+//         echo "</div>";
+//       } else {
+//         echo "<div class='img-message'>";
+//         echo "Sorry, there was an error uploading your file.";
+//         echo "</div>";
+//       }
+//     }
+//       }
 function addRecipeController(){
         if(isset($_POST['name'], $_POST['additionDate'], $_POST['category'], $_POST['ingredients'], $_POST['preparation'], $_FILES["fileToUpload"])){
             
