49821e77c742c8457eb62518077f8dc70ca34ccf
irensula <irinawebdev@gmail.com>
2024-05-20 11:59:37 +0300

    Add files via upload

--- phpharjoitukset9/test.php
+++ phpharjoitukset9/test.php
@@ -22,35 +22,54 @@
 
     <script>
         
-        //  function changeBackgroundColor() {
-        //     const inputContainer = document.querySelectorAll(".answer-choice");
-        //     for (let i = 0; i < inputContainer.length; i++) {
-        //         let correct = document.getElementById('correct').value;
-        //         let userChoice = inputContainer[i].checked;
-        //         console.log(correct);
-        //         if (correct === userChoice) {
-        //             correct.parentElement.classList.add('rightAnswer');
-        //             document.getElementById("result").innerHTML = "You have selected: " + inputContainer[i].value;
-        //             event.preventDefault();
-        //         } else {
-        //             userChoice.parentElement.classList.add('wrongAnswer');
-        //             event.preventDefault();
-        //         }
+         function changeBackgroundColor() {
+            let correctAnswer = document.getElementById("correct");
+            let allChoices = document.querySelectorAll('.answer-choice');
+            let correct = correctAnswer.value;
+            event.preventDefault();
+            for (let i = 0; i < allChoices.length; i++) {
+                let correct = correctAnswer.value;
+                let userChoice = allChoices[i].checked;
+
+                if(userChoice) {
+                    console.log(correct);
+                }
+            }
+            
+            // for (let i = 0; i < allChoices.length; i++) {
+            //     
+            //     let userChoice = allChoices[i].checked;
+
+            //     if (correct === userChoice) {
+            //         event.preventDefault();
+            //         correct.classList.add('rightAnswer');
+            //         document.getElementById("result").innerHTML = "You have selected: " + inputContainer[i].value;
+                    
+                    
+                // } else {
+                //     userChoice.parentElement.classList.add('wrongAnswer');
+                    
+                }
         // }
         // }
 
-        function selectedButton() {
-            const inputContainer = document.querySelectorAll(".answer-choice");
-            for (let i = 0; i < inputContainer.length; i++) {
-            if (inputContainer[i].checked) {
-                inputContainer[i].parentElement.classList.add('selected');
-                document.getElementById("result").innerHTML = "You have selected: " + inputContainer[i].value;
-            } else {
-                inputContainer[i].parentElement.classList.remove('selected');
-                event.preventDefault();
-            }
-        }
-    }
+    //     function selectedButton() {
+    //         let inputContainer = document.querySelectorAll(".answer-choice");
+    //         let correct = document.getElementById('correct').value;
+    //         console.log(correct);
+    //         for (let i = 0; i < inputContainer.length; i++) {
+                
+    //         if (inputContainer[i].checked) {
+    //             inputContainer[i].parentElement.classList.add('selected');
+    //             document.getElementById("result").innerHTML = "You have selected: " + inputContainer[i].value;
+                
+    //             document.getElementById("score").innerHTML = "Your score: ";
+    //         } else {
+    //             inputContainer[i].parentElement.classList.remove('selected');
+    //             event.preventDefault();
+    //         }
+    //     }
+    // }
     
     </script>
 </head>
@@ -60,48 +79,49 @@
         <h1>TIETOVISA</h1>
         <h2>How well do you know Friends?</h2>
         
-        <form action="index_.php" method="get">
+        <form action="test.php" method="get">
             <ul>
                 <?php
                 
-                    $questions = getTenQuestions();
+                    $question = getQuestionById(1);
 
-                    foreach($questions as $question) {
+                    // foreach($questions as $question) {
                         
-                    echo "<li>" . $question["question"];
+                    // echo "<li>" . $question["question"];
                     ?>
                     </li>
-                        <div id="input-container">
+                        <div id="input-container_1">
                             <input class="answer-choice" type='radio' id='answer_1' name='choice' value='<?=$question["answerA"]?>' />
                             <label for='choice'><?= $question["answerA"] ?></label>
                         </div>
                         
-                        <div id="input-container">
+                        <div id="input-container_2">
                             <input class="answer-choice" type='radio' id='answer_2' name='choice' value='<?=$question["answerB"]?>' />
                             <label for='choice'><?= $question["answerB"] ?></label>
                         </div>
                         
-                        <div id="input-container">
+                        <div id="input-container_3">
                             <input class="answer-choice" type='radio' id='answer_3' name='choice' value='<?=$question["answerC"]?>' />
                             <label for='choice'><?= $question["answerC"] ?></label>
                         </div>
                         
-                        <div id="input-container">
+                        <div id="input-container_4">
                             <input class="answer-choice" type='radio' id='answer_4' name='choice' value='<?=$question["answerD"]?>' />
                             <label for='choice'><?= $question["answerD"] ?></label>
                         </div>
+                        <div id="input-container_5" class="hidden">
+                            <input class="correct" type='hidden' id='correct' name='correct' value='<?=$question["correct"]?>' />
+                            <label for='correct'></label>
+                        </div>
 
-                        <input class="correct" type='hidden' id='correct' name='correct' value='<?=$question["correct"]?>' />
-                        <label for='correct'></label>
-                        
-                        <div id="result"></div>
+                        <div id="result">Answer:</div>
+                        <div id="score">Score:</div>
                         
                         <button id="submit" class="button" type="submit" onclick="changeBackgroundColor()">Tarkista</button>
                                   
-                <?php } ?>
+                <?php //} ?>
             </ul>
         </form>
-        
 
                     <?php
                         
